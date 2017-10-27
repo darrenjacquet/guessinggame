46fc762a6a47ab021a4dba79b9535dd3ebfea76a
djacquet <djacquet@users.noreply.github.com>
2017-10-26 22:18:19 -0600

    Added looping and other conditional logic

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,31 @@
-echo "How many files do you think are in the current directory?"
-echo "(Please make your best guess):"
-read response
-
+file_count=$(ls -1 | wc -l)
+loop_count=0
 # Test input
-echo "You entered: $response"
+echo "Starting script in $(pwd) with code $file_count."
+
+keep_trying=0
+while [[ $keep_trying -eq 0 ]] ; do
+    if [[ $loop_count -eq 0 ]] ; then
+        echo "How many files do you think are in the current directory?"
+        echo "(Please make your best guess):"
+
+    else
+        echo "Please make your best guess again:"
+
+    fi
+    read response
+#    clear
+
+    if [[ $response -gt $file_count ]] ; then
+        echo "Sorry, your guess of $response was a little high."
+
+    elif [[ $response -lt $file_count ]] ; then
+        echo "Sorry, your guess of $response was a little low."
+
+    else
+        echo "Congratulations!!! your guess of $response was spot on."
+        keep_trying=1
+
+    fi
+
+done
