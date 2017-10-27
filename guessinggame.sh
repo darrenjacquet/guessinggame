30cecd97cc6729aacd8039b9ecdb930b10cd6663
djacquet <djacquet@users.noreply.github.com>
2017-10-26 22:30:23 -0600

    Split code into functions to make more modular

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,21 +1,32 @@
-file_count=$(ls -1 | wc -l)
-loop_count=0
-# Test input
-echo "Starting script in $(pwd) with code $file_count."
+function main {
+    file_count=$(ls -1 | wc -l)
+    loop_count=0
+    # Test input
+    echo "Starting script in $(pwd) with code $file_count."
 
-keep_trying=0
-while [[ $keep_trying -eq 0 ]] ; do
+    keep_trying=0
+    while [[ $keep_trying -eq 0 ]] ; do
+        show_prompt $loop_count
+        read response
+
+        process_response $response
+
+    done
+}
+
+function show_prompt {
     if [[ $loop_count -eq 0 ]] ; then
         echo "How many files do you think are in the current directory?"
         echo "(Please make your best guess):"
+        loop_count=1
 
     else
         echo "Please make your best guess again:"
 
     fi
-    read response
-#    clear
+}
 
+function process_response {
     if [[ $response -gt $file_count ]] ; then
         echo "Sorry, your guess of $response was a little high."
 
@@ -26,6 +37,7 @@ while [[ $keep_trying -eq 0 ]] ; do
         echo "Congratulations!!! your guess of $response was spot on."
         keep_trying=1
 
-    fi
+   fi
+}
 
-done
+main
